d59b81adfcfa1bee1f0963c6557be88b813fe94b
hk <2350049942@qq.com>
2024-01-10 18:57:15 +0800

    completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A.rest == null){
+            A.rest=B;
+            return A;
+        }
+        A.rest = dcatenate(A.rest,B);
+        return A;
     }
 
     /**
@@ -91,7 +96,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A.rest == null){
+            return new IntList(A.first,B);
+        }
+        return new IntList(A.first,catenate(A.rest,B));
     }
 
 
