98559137b63789c326dd4ef1d0ab6b4f7b6f6e17
hk <2350049942@qq.com>
2024-02-15 21:37:03 +0800

    update Arraydeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,15 +1,15 @@
-public class ArrayDeque {
-    int[] items;
-    int nextFirst;
-    int nextLast;
-    int size;
-    public ArrayDeque(){
-        items = new int[8];
+public class ArrayDeque<Glorp> {
+    private Glorp[] items;
+    private int nextFirst;
+    private int nextLast;
+    private int size;
+    public ArrayDeque() {
+        items = (Glorp []) new Object[8];;
         size = 0;
         nextFirst = 3;
         nextLast = 4;
     }
-    public ArrayDeque(ArrayDeque other){
+    public ArrayDeque(ArrayDeque<Glorp> other) {
         size = other.size;
         nextLast = other.nextLast;
         nextFirst = other.nextFirst;
@@ -19,13 +19,13 @@ public class ArrayDeque {
     }
     public  int size(){return size;}
     public void resize(){
-        int[] a= new int[items.length*2];
+        Glorp[] a = (Glorp []) new Object[items.length*2];
         System.arraycopy(items,0,a,0,size);
         items = a;
         nextFirst = items.length-1;
         nextLast = size;
     }
-    public void addFirst(int val){
+    public void addFirst(Glorp val){
         if(size == items.length){
             resize();
         }
@@ -33,7 +33,7 @@ public class ArrayDeque {
         size += 1;
         nextFirst = (nextFirst + items.length)%items.length;
     }
-    public  void addLast(int val){
+    public  void addLast(Glorp val){
         if(size == items.length){
             resize();
         }
@@ -49,10 +49,10 @@ public class ArrayDeque {
         nextLast--;
         size -= 1;
     }
-    public int get(int index){
+    public Glorp get(int index){
         return items[index];
     }
-    public int getLast(){
+    public Glorp getLast(){
         int i = nextLast-1;
         return items[i];
     }
